c72297f30ee754161323bcd5b5b0426bfc698140
zachs <zachs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-21 21:39:15 +0000

    why am i using svn wtf
    git-svn-id: https://plugins.svn.wordpress.org/wprogress-bar/trunk@1203514 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wprogress.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WProgress
+Plugin URI: https://github.com/zackify/WProgress
+Description: Enhance the readability of your blog
+Version: 0.1
+Author: Zach Silveira & Alex Kang
+Author URI: https://github.com/zackify
+*/
+require_once "actions.php";
+require_once "filters.php";
